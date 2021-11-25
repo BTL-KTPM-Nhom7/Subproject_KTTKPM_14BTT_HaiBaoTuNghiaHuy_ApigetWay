2f32363866c2782bda6393fe45838f4600f29669
IBM <thieuquangnghia2000@gmail.com>
2021-11-25 22:17:07 +0700

    aipgetway

---
+++ src/main/java/com/example/apigetway/ApigetwayApplication.java
@@ -0,0 +1,14 @@
+package com.example.apigetway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ApigetwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApigetwayApplication.class, args);
+    }
+
+}
